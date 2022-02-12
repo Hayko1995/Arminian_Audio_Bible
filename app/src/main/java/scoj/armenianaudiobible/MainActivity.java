087d010c823d2fc82be172905f98b415d5ad80ea
Hayko1995h <haykhakobyan95@outlook.com>
2022-02-12 18:34:51 +0400

    .

---
+++ app/src/main/java/scoj/armenianaudiobible/MainActivity.java
@@ -0,0 +1,125 @@
+package scoj.armenianaudiobible;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+public class MainActivity extends AppCompatActivity {
+    ArrayAdapter<String> adapter;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        boolean chack = false;
+
+
+
+        final ListView gluxner_list = (ListView) findViewById(R.id.list_item);
+        final String[] gluxner_arr = getResources().getStringArray(R.array.gluxner);
+        final String[] erger = getResources().getStringArray(R.array.erger);
+        final String[] cucak = getResources().getStringArray(R.array.cucak);
+
+
+        adapter = new ArrayAdapter<>(
+                MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                gluxner_arr);
+
+        if (chack == false) {
+            chack = isReadStoragePermissionGranted();
+        }
+
+
+
+        gluxner_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(MainActivity.this, Player.class);
+                intent.putExtra("glux", gluxner_list.getItemAtPosition(i).toString());
+                intent.putExtra("erg", erger[i].toString());
+                intent.putExtra("cucak", cucak[i].toString());
+                startActivity(intent);
+            }});
+
+        // присваиваем адаптер списку
+        gluxner_list.setAdapter(adapter);
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean isReadStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                return true;
+            } else {
+
+
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 3);
+                return false;
+            }
+        } else { //permission is automatically granted on sdk<23 upon installation
+
+            return true;
+        }
+    }
+
+    public boolean isWriteStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+
+                return true;
+            } else {
+
+
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+                return false;
+            }
+        } else { //permission is automatically granted on sdk<23 upon installation
+
+            return true;
+        }
+    }
+}
---
+++ app/src/main/java/scoj/armenianaudiobible/Player.java
@@ -0,0 +1,189 @@
+package scoj.armenianaudiobible;
+
+import android.app.DownloadManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+
+import static java.lang.Integer.parseInt;
+
+
+public class Player extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer = new MediaPlayer();
+
+    private ImageButton play_button = null;
+    private ImageButton prev_button = null;
+    private ImageButton next_button = null;
+    private ListView listView = null;
+    String selected = "1";
+    String url;
+    String fileName;
+    String carent_file;
+    String pathDir = "/Android/data/scoj.armenianaudiobible/files/Music";
+    String erg1;
+    int cank;
+    Long referance = Long.valueOf(0);
+    DownloadManager downloadManager;
+
+    int i;
+
+    @Override
+    public void onBackPressed() {
+        mediaPlayer.reset();
+        super.onBackPressed();
+    }
+    View.OnClickListener play = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            fileName = erg1 + "_" + selected + ".mp3";
+            try {
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.pause();
+                    mediaPlayer.reset();
+                    play_button.setImageResource(android.R.drawable.ic_media_play);
+                } else {
+                    //listView.setEnabled(false);
+
+                    //prev_button.setEnabled(false);
+                    //next_button.setEnabled(false);
+                    carent_file = Environment.getExternalStorageDirectory().getPath().toString() + pathDir + "/" + fileName;
+                    File f = new File(carent_file);
+                    if (f.exists()) {
+                        mediaPlayer.setDataSource(carent_file);
+
+                        mediaPlayer.prepare();
+                        mediaPlayer.start();
+
+                        listView.setEnabled(true);
+                        prev_button.setEnabled(true);
+                        next_button.setEnabled(true);
+                        play_button.setImageResource(android.R.drawable.ic_media_pause);
+                    } else {
+
+                        downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                        //request.setDestinationInExternalPublicDir(Environment.getExternalStorageState(), erg1 + "_" + selected + ".mp3");
+                        request.setDestinationInExternalFilesDir(Player.this, Environment.DIRECTORY_MUSIC, fileName);
+                        referance = downloadManager.enqueue(request);
+
+
+                        carent_file = Environment.getExternalStorageDirectory().getAbsolutePath().toString() + pathDir + "/" + fileName;
+                        Log.i("aaa", carent_file);
+                        //mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                        //if(Long.valueOf(0) != referance){play_button.performClick();referance = Long.valueOf(0);}
+
+                    }
+
+                }
+
+
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext(), "Inetrnet error", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    };
+    View.OnClickListener next = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int a = Integer.parseInt(selected);
+            if (a == cank) {
+                selected = String.valueOf(1);
+            } else {
+                selected = String.valueOf(a + 1);
+            }
+            if (mediaPlayer.isPlaying()) play_button.performClick();
+            url = "https://www.derekprincearmenia.com/audiofiles/hy/" + erg1 + "/" + selected + ".mp3";
+            play_button.performClick();
+
+
+        }
+    };
+    View.OnClickListener prev = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int a = Integer.parseInt(selected);
+            if (a == 1) {
+                selected = String.valueOf(cank);
+            } else {
+                selected = String.valueOf(a - 1);
+            }
+            if (mediaPlayer.isPlaying()) play_button.performClick();
+            url = "https://www.derekprincearmenia.com/audiofiles/hy/" + erg1 + "/" + selected + ".mp3";
+            play_button.performClick();
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+
+
+        play_button = (ImageButton) findViewById(R.id.play);
+        prev_button = (ImageButton) findViewById(R.id.prev);
+        next_button = (ImageButton) findViewById(R.id.next);
+        listView = (ListView) findViewById(R.id.listView);
+
+
+        Bundle bundle = getIntent().getExtras();
+        String glux = bundle.getString("glux");
+        String erg = bundle.getString("erg");
+        String cucak = bundle.getString("cucak");
+
+        erg1 = erg;
+
+        cank = parseInt(cucak);
+        String arr[] = new String[cank];
+        for (i = 0; i < cank; i++) {
+            arr[i] = String.valueOf(i + 1);
+        }
+
+        url = "https://www.derekprincearmenia.com/audiofiles/hy/" + erg + "/" + selected + ".mp3";
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, arr);
+        listView.setAdapter(adapter);
+
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(glux);
+        play_button.setOnClickListener(play);
+        next_button.setOnClickListener(next);
+        prev_button.setOnClickListener(prev);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                selected = String.valueOf(position + 1);
+
+
+                if (mediaPlayer.isPlaying()) play_button.performClick();
+                url = "https://www.derekprincearmenia.com/audiofiles/hy/" + erg1 + "/" + selected + ".mp3";
+                play_button.performClick();
+
+            }
+
+        });
+    }
+}
+
+
+
+
